6fa01a15b627ffbd8b3219385a50cd6eb637a299
Neal Bayya <neal.bayya@gmail.com>
2020-08-02 18:03:31 -0400

    updated with comments table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
+  validates :title, :body, presence: true
+  validates :title, length: { maximum: 150 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   has_many :posts
+  has_many :comments
 end
